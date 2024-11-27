73cbe84f98b9e72e0be7f2f52d22c2c07a95a167
Michu <michal.dakowicz@infotech.edu.pl>
2024-11-27 11:26:13 +0100

    lekkie przepisanie kodu

---
+++ GymShop/alledrogo/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Firma(models.Model):
+    id = models.AutoField(primary_key=True)
+    nazwa = models.CharField(max_length=256)
+    opis = models.TextField()
+    ikona = models.ImageField(upload_to='media/ikony_firm', null=True, blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Kategoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nazwa = models.CharField(max_length=256)
+    opis = models.TextField()
+
+    def __str__(self):
+        return self.nazwa
+
+class Produkt(models.Model):
+    id = models.AutoField(primary_key=True)
+    nazwa = models.CharField(max_length=256)
+    opis = models.TextField()
+    cena = models.FloatField()
+    srednia_ocen = models.FloatField(default=0)
+    kategoria = models.ForeignKey(Kategoria, on_delete=models.CASCADE)
+    firma = models.ForeignKey(Firma, on_delete=models.CASCADE, null=True, blank=True)
+    zdjecie = models.ImageField(upload_to='media/zdjecia_produktow', null=True, blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Zamowienie(models.Model):
+    id = models.AutoField(primary_key=True)
+    produkty = models.ManyToManyField(Produkt)
+    klient = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_zamowienia = models.DateTimeField(auto_now_add=True)
+    data_dostarczenia = models.DateTimeField()
+    adres_dostawy = models.TextField()
+
+    def __str__(self):
+        return str(self.klient) + ' ' + str(self.data_zamowienia)
+
+class Ocena(models.Model):
+    id = models.AutoField(primary_key=True)
+    produkt = models.ForeignKey(Produkt, on_delete=models.CASCADE)
+    klient = models.ForeignKey(User, on_delete=models.CASCADE)
+    ocena = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    komentarz = models.TextField()
+
+    def __str__(self):
+        return str(self.produkt) + ' ' + str(self.ocena) + ' ' + str(self.komentarz)
---
+++ GymShop/alledrogo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home, login, register, profile, logout
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
